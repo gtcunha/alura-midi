b8932297ab63a868a9cfc6267879ab8dcddf4567
gtcunha <gustavothebaldi@gmail.com>
2023-02-04 20:04:04 -0300

    Commit inicial - Enviando arquivos para o repositório

---
+++ main.js
@@ -0,0 +1,19 @@
+/ Função para tecla Pom /
+
+function tocaSomPom () {
+    document.querySelector('#som_tecla_pom').play();
+} 
+
+/ Comando para ação do botão Pom /
+
+/ document.querySelector('.tecla_pom').onclick = tocaSomPom; /
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
+const listaDeSons = document.querySelectorAll('.som_tecla_pom');
+
+
+for (var cont=0; cont <9; cont++) {
+
+}
+
+
