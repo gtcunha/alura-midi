1e9f0eb84903bd0d2854c142e61e9ed9c92bd476
gtcunha <gustavothebaldi@gmail.com>
2023-02-27 23:10:17 -0300

    Adicionando uma condição para a entrada de teclas.

--- main.js
+++ main.js
@@ -58,14 +58,20 @@ for (let contador = 0 ; contador < listaDeTeclas.length ; contador++) {
         tocaSom(idAudio);
     }
 
-    tecla.onkeydown = function () {
-        tecla.classList.add('ativa');
-        
+    tecla.onkeydown = function (evento) {
+
+        console.log(evento.code === 'Space');
+
+        if (evento.code == 'Space') {
+            tecla.classList.add('ativa');
+        }
     }
     
     tecla.onkeyup = function () {
         tecla.classList.remove('ativa');
     }
+
+    
 
 }
 
